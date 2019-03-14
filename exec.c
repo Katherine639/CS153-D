809347c4f716cb9496ef419bcacf37ba329060f2
Katherine639 <kway001@ucr.edu>
2019-03-14 13:14:31 -0700

    Update exec.c

--- exec.c
+++ exec.c
@@ -62,23 +62,12 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
-  
-  //LAB 3 CHANGES
-  
-  /*
+  sz = PGROUNDUP(sz);
   if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
     goto bad;
   clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
   sp = sz;
-  */
-  
-  //Allocate stack (STACKTOP - PGSIZE)
-  sz = PGROUNDUP(sz);
-  if (allocuvm(pgdir, STACKTOP - PGSIZE, STACKTOP) == 0)
-	goto bad;
-  sp = STACKTOP;
-  curproc->numPage = 1;
-  
+
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
     if(argc >= MAXARG)
